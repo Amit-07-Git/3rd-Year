e9f89011d52296d75dc66542de27c132ce7ba066
Amit Kumar <146549284+Amit-07-Git@users.noreply.github.com>
2026-02-09 15:31:59 +0530

    Add files via upload

---
+++ 6th SEM/GPWC/Lab Code/first.cpp
@@ -0,0 +1,13 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
+
+int main(){
+  VideoMode vm(1920, 1080);
+  RenderWindow window(vm, "Circle");
+  while(window.isOpen()){
+  //handle the players
+  //updates scene
+  //draw scene
+  }
+  return 0;
+}
