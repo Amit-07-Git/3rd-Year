76c9a383fcb3669717e8d305a78882225bf4fcb1
Amit Kumar <146549284+Amit-07-Git@users.noreply.github.com>
2026-02-18 17:00:22 +0530

    Add files via upload

---
+++ 6th SEM/GPWC/Lab Code/GAME/timber.cpp
@@ -0,0 +1,11 @@
+#include<SFML/Graphics.hpp>
+using namespace sf;
+int main(){
+  VideoMode vm(1920,1080);
+  RenderWindow window(vm, "Timber");
+  while(window.isOpen()){
+  }
+  
+  
+  return 0;
+}
