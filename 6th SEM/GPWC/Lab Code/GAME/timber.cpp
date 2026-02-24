e8ec689f61958159bc81874e7f0dfab9101b168a
Amit Kumar <146549284+Amit-07-Git@users.noreply.github.com>
2026-02-24 16:47:01 +0530

    Add files via upload

--- 6th SEM/GPWC/Lab Code/GAME/timber.cpp
+++ 6th SEM/GPWC/Lab Code/GAME/timber.cpp
@@ -3,9 +3,48 @@ using namespace sf;
 int main(){
   VideoMode vm(1920,1080);
   RenderWindow window(vm, "Timber");
+  View view(FloatRect(0,0,1920,1080));
+  
+  Texture texturebackground;
+  Texture texturetree;
+  Texture texturebee;
+  
+  texturebackground.loadFromFile("/home/iteradmin/Music/6th Semester/GDWC/GAME/Timber_prereq/graphics/background.png"); 
+  texturetree.loadFromFile("/home/iteradmin/Music/6th Semester/GDWC/GAME/Timber_prereq/graphics/tree.png");
+  texturebee.loadFromFile("/home/iteradmin/Music/6th Semester/GDWC/GAME/Timber_prereq/graphics/bee.png");
+  
+  Sprite spriteBackground;
+  Sprite spriteTree;
+  Sprite spriteBee;
+  
+  spriteBackground.setTexture(texturebackground);
+  spriteBackground.setPosition(0,0);
+  
+  spriteTree.setTexture(texturetree);
+  spriteTree.setPosition(810, 0);
+  
+  spriteBee.setTexture(texturebee);
+  spriteBee.setPosition(10, 800);
+  
   while(window.isOpen()){
+    //handle the players input
+    Event event;
+    while(window.pollEvent(event)){
+      if (event.type == Event::Closed){
+        window.close();
+      }
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Escape)){
+      window.close();
+    }
+  //updates scene
+  //draw scene
+  window.clear();
+  window.draw(spriteBackground);
+  window.draw(spriteTree);
+  window.draw(spriteBee);
+  window.display();
+  window.setView(view);
   }
-  
-  
   return 0;
 }
