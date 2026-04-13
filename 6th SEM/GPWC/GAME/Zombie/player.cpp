b7b1632487fa1f6559bb0537059ddf17bfb27969
Amit Kumar <146549284+Amit-07-Git@users.noreply.github.com>
2026-04-13 17:15:13 +0530

    Add files via upload

--- 6th SEM/GPWC/GAME/Zombie/player.cpp
+++ 6th SEM/GPWC/GAME/Zombie/player.cpp
@@ -1,4 +1,3 @@
-
 #pragma once
 #include<SFML/Graphics.hpp>
 #include<cmath>
@@ -21,6 +20,7 @@ class Player{
     int m_MaxHealth;
     Time m_LastHit;
     float m_Speed;
+    IntRect m_Arena;
   public:
     Player();
     void spawn(IntRect arena, Vector2f resolution, int tileSize);
@@ -55,6 +55,94 @@ Player::Player(){
   m_Sprite.SetPosition(1920/2.0f, 1080/2.0f);
 }
 
+void Player::spawn(IntRect arena, Vector2f resolution, int tileSize){
+  m_Position.x = arena.width/2;
+  m_Position.y = arena.height/2;
+  m_Arena.left = arena.left;
+  m_Arena.top = arena.top;
+  m_Arena.width = arena.width;
+  m_Arena.height = arena.height;
+  m_TileSize = tileSize;
+  m_Resolution.x = resolution.x;
+  m_Resolution.y = resolution.y;
+  m_Sprite.setPosition(m_Position);
+}
+
+bool Player::hit(Time timehit){
+  if(timehit.asMilliseconds() - m_LastHit.asMilliseconds() > 200){
+    m_LastHit = timehit;
+    m_Health -= 10
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+FloatRect Player::getPosition(){
+  return m_Sprite.getGlobalBounds();
+}
+
+Vector2f Player::getCenter(){
+  return m_position;
+}
+
+float Player::getRotation(){
+  return m_Sprite.getPosition();
+}
+
+Sprite Player::getSprite(){
+  return m_Sprite;
+}
+
+int Player::getHealth(){
+  return m_Health;
+}
+
+void Player::moveLeft(){
+  m_leftPressed = true;
+}
+void Player::moveRight(){
+  m_RightPressed = true;
+}
+void Player::moveUp(){
+  m_UpPressed = true;
+}
+void Player::moveDown(){
+  m_DownPressed = true;
+}
+void Player::stopLeft(){
+  m_leftPressed = false;
+}
+void Player::stopRight(){
+  m_RightPressed = false;
+}
+void Player::stopUp(){
+  m_UpPressed = false;
+}
+void Player::stopDown(){
+  m_DownPressed = false;
+}
+
+void Player::update(float elapsedTime, Vector2i mousePosition){
+  if(m_UpPressed){
+    m_Position.y -= m_Speed * elapsedTime;
+  }
+  if(m_DownPressed){
+    m_Position.y += m_Speed * elapsedTime;
+  }
+  if(m_LeftPressed){
+    m_Position.x -= m_Speed * elapsedTime;
+  }
+  if(m_RightPressed){
+    m_Position.x += m_Speed * elapsedTime;
+  }
+  m_Sprite.setPosition(m_Position);
+  
+  if(m_Position.x > m_Arena.width - m_TileSize){
+    
+  }
+}
 
 int main(){
 
