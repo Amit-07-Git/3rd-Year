2e5d19feb65671c83486b48450f573138a7b7bfc
Amit Kumar <146549284+Amit-07-Git@users.noreply.github.com>
2026-05-06 15:58:28 +0530

    Add files via upload

--- 6th SEM/GPWC/GAME/Zombie/zombieArena.cpp
+++ 6th SEM/GPWC/GAME/Zombie/zombieArena.cpp
@@ -1,41 +1,64 @@
-//g++ zombieArena.cpp -o zombie -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+// g++ zombieArena.cpp -o zombie -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
-#include<SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "player.cpp"
+
 using namespace sf;
-int main(){
-  Vector2f resolution;
-  resolution.x = VideoMode::getDesktopMode().width;
-  resolution.y = VideoMode::getDesktopMode().height;
-  RenderWindow window(VideoMode(resolution.x, resolution.y), "Zombie");
-  
-  RectangleShape windowRect(Vector2f(resolution.x, resolution.y));
-  windowRect.setFillColor(Color::Green);
-  
-  while(window.isOpen()){
-    Event event;
+
+int main() {
+    enum class state {PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
+    
+    Vector2f resolution;
+    resolution.x = VideoMode::getDesktopMode().width;
+    resolution.y = VideoMode::getDesktopMode().height;
+    RenderWindow window(VideoMode(resolution.x, resolution.y), "Zombie");
+
+    Vertex lineX[2];
+    lineX[0].position = Vector2f(0, resolution.y / 2);
+    lineX[0].color = Color::Green;
+    lineX[1].position = Vector2f(resolution.x, resolution.y / 2);
+    lineX[1].color = Color::Green;
+
+    Vertex lineY[2];
+    lineY[0].position = Vector2f(resolution.x / 2, 0);
+    lineY[0].color = Color::Green;
+    lineY[1].position = Vector2f(resolution.x / 2, resolution.y);
+    lineY[1].color = Color::Green;
+
+    RectangleShape windowRect(Vector2f(resolution.x, resolution.y));
+    windowRect.setOutlineThickness(5);
+    windowRect.setFillColor(Color::Red);
+
+    RectangleShape rectArena;
+    View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+    Clock clock;
+    Time gameTimeTotal;
+    Vector2f mouseWorldPosition;
+    Vector2i mouseScreenPosition;
     bool paused = false;
-    while (window.pollEvent(event)) {
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
             if (event.type == Event::Closed) {
                 window.close();
-            }       
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-            window.close();
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Return)) {
-            paused = true;
-    }
-    if (Keyboard::isKeyPressed(Keyboard::Return)) {
-            paused = false;
-    }
-    
-            
-            
-    window.clear();          //window.clear(Color::Green);
-    window.draw(windowRect);
-    window.display();
-  }
-  return 0;
-}
+            }
+            if (event.type == Event::KeyPressed) {
+                if (event.key.code == Keyboard::Escape) {
+                    window.close();
+                }
+                if (event.key.code == Keyboard::Return) {
+                    paused = !paused;
+                }
+            }
+        }
 
+        window.clear();
+        window.draw(windowRect);
+        window.draw(lineX, 2, Lines);
+        window.draw(lineY, 2, Lines);
+        window.display();
+    }
+    return 0;
+}
